2cf2c78919d832db09f616093b2c39fe6994baa9
Gabe Blair <gabe@castironcoding.com>
2017-01-05 15:48:40 -0800

    [U] Make a few TYPO3 7 compat changes

--- Configuration/TCA/Type.php
+++ Configuration/TCA/Type.php
@@ -20,6 +20,7 @@ $TCA['tx_cicslide_domain_model_type'] = array(
 			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.language',
 			'config' => array(
 				'type' => 'select',
+                'renderType' => 'selectSingle',
 				'foreign_table' => 'sys_language',
 				'foreign_table_where' => 'ORDER BY sys_language.title',
 				'items' => array(
@@ -34,6 +35,7 @@ $TCA['tx_cicslide_domain_model_type'] = array(
 			'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.l18n_parent',
 			'config' => array(
 				'type' => 'select',
+                'renderType' => 'selectSingle',
 				'items' => array(
 					array('', 0),
 				),
